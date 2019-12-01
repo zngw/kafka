e4e6a0b872dbca7e6fec8616dbbdf061ba37f3d4
guoke3915 <guoke3915@163.com>
2019-12-02 01:28:46 +0800

    kafka封装

--- README.md
+++ README.md
@@ -1,2 +1,65 @@
 # kafka
-kafka的使用
+## 生成产测试producer.go
+```go
+package main
+
+import (
+	"github.com/zngw/log"
+	"github.com/zngw/kafka"
+	"os/signal"
+	"runtime"
+	"syscall"
+)
+
+func main() {
+	// 初始化日志
+	log.Init(nil)
+
+	// 初始化生产生
+	kafka.InitProducer("192.168.1.29:9092")
+
+	// 发送测试消息
+	kafka.Send("Test","This is Test Msg")
+	kafka.Send("Test","Hello Guoke")
+
+	// 关闭
+	kafka.Close()
+
+	signal.Ignore(syscall.SIGHUP)
+	runtime.Goexit()
+}
+```
+## 消费者测试consumer.go
+```go
+package main
+
+import (
+	"github.com/zngw/log"
+	"github.com/zngw/kafka"
+	"os/signal"
+	"runtime"
+	"syscall"
+)
+
+func main() {
+	// 初始化日志
+	log.Init(nil)
+
+	// 初始化消费者
+	kafka.InitConsumer("127.0.0.1:9092")
+
+	// 监听
+	go func() {
+		kafka.LoopConsumer("Test", TopicCallBack)
+	}()
+
+	signal.Ignore(syscall.SIGHUP)
+	runtime.Goexit()
+}
+
+func TopicCallBack(data []byte) {
+	log.Trace("kafka", "Test:"+string(data))
+}
+```
+## 执行结果
+![](result.png)
---
+++ kafka.go
@@ -0,0 +1,90 @@
+package kafka
+
+import (
+	"strings"
+	"github.com/Shopify/sarama"
+	"github.com/zngw/log"
+)
+
+const (
+	TopicBusiness = "business"
+	TopicSystem   = "system"
+)
+
+var (
+	producer sarama.AsyncProducer
+	consumer sarama.Consumer
+)
+
+// 消费者回调函数
+type ConsumerCallback func(data []byte)
+
+// 初始化消费者
+func InitConsumer(hosts string) {
+	config := sarama.NewConfig()
+	client, err := sarama.NewClient(strings.Split(hosts, ","), config)
+	if err != nil {
+		log.Error("unable to create kafka client: %q", err)
+	}
+
+	consumer, err = sarama.NewConsumerFromClient(client)
+	if err != nil {
+		log.Error(err)
+	}
+}
+
+// 消费者循环
+func LoopConsumer(topic string, callback ConsumerCallback) {
+	partitionConsumer, err := consumer.ConsumePartition(topic, 0, sarama.OffsetNewest)
+	if err != nil {
+		log.Error(err)
+		return
+	}
+	defer partitionConsumer.Close()
+
+	for {
+		msg := <-partitionConsumer.Messages()
+		if callback != nil {
+			callback(msg.Value)
+		}
+	}
+}
+
+// 初始化生产者
+func InitProducer(hosts string) {
+	config := sarama.NewConfig()
+	client, err := sarama.NewClient(strings.Split(hosts, ","), config)
+	if err != nil {
+		log.Error("unable to create kafka client: %q", err)
+	}
+	producer, err = sarama.NewAsyncProducerFromClient(client)
+	if err != nil {
+		log.Error(err)
+	}
+}
+
+// 发送消息
+func Send(topic, data string) {
+	producer.Input() <- &sarama.ProducerMessage{Topic: topic, Key: nil, Value: sarama.StringEncoder(data)}
+	log.Trace("kafka", "Produced message: ["+ data+"]")
+}
+
+func SendBusiness(data string) {
+	Send(TopicBusiness, data)
+}
+
+func SendSystem(data string) {
+	Send(TopicSystem, data)
+}
+
+// 关闭
+func Close() {
+	log.Trace("kafka","Close")
+	if producer != nil {
+		producer.Close()
+	}
+
+	if consumer != nil {
+		consumer.Close()
+	}
+}
